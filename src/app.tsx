7577411873f854f14aa217a1b8ec6729b8d49261
Pedro Henrique da Silva Melo <pedrodsm1819@gmail.com>
2024-02-07 22:47:30 -0300

    Propriedades e modal de apagar nota

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: 'Hello World'
+}
+
 export function App() {
   return ( 
     <div className="mx-auto max-w-6xl my-12 space-y-6"> 
@@ -19,12 +24,9 @@ export function App() {
         <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'> 
           <NewNoteCard/>
     
-          <NoteCard/> 
-          <NoteCard/> 
-          <NoteCard/> 
-          <NoteCard/> 
-          <NoteCard/> 
-          <NoteCard/> 
+          <NoteCard note={note} /> 
+          {/* <NoteCard date={new Date()} content="Teste" />  */}
+
             
         </div>
 
